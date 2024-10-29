d70734bf7309cb98d088392d775dff8a371717fb
LuisFernandoMazo <luisfernandomazoc18@gmail.com>
2024-10-29 18:28:27 -0500

    add route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { PublicacionesComponent } from '../app/features/publicaciones/publicaciones.component';
 import { UsuarioComponent } from '../app/features/usuario/usuario.component';
-import {LoginComponent} from '../app/features//auth/login/login.component';
+import { LoginComponent } from '../app/features//auth/login/login.component';
+import { RegisterComponent } from '../app/features//auth/register/register.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/publicaciones', pathMatch: 'full' },
-    { path:'login', component: LoginComponent},
-    { path:'publicaciones', component: PublicacionesComponent},
-    { path:'usuarioProfile', component: UsuarioComponent}
+  { path: '', redirectTo: '/publicaciones', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'publicaciones', component: PublicacionesComponent },
+  { path: 'usuarioProfile', component: UsuarioComponent },
 ];
-
